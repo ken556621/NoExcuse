feb437b0df91846ff6d6cb183c370ecaf960dfd1
ken <ken556621@gmail.com>
2020-03-25 06:57:26 +0800

    Refactor Auth

--- src/components/common/auth.js
+++ src/components/common/auth.js
@@ -7,7 +7,6 @@ export default function(ComposedClass){
     class Auth extends Component {
         constructor(props) {
               super(props);
-              console.log(props)
         }
   
         componentDidMount(){
